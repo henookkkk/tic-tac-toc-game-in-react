bf17a308d550950818d7e53c51287c06e09dce3b
Henok Dereje <henzdereje@gmail.com>
2026-03-16 06:47:29 -0700

    i opened a folder for components and added player component and used it in the app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,13 @@
+import Player from "./components/Player"
+
 
 function App() {
   return (
     <main>
       <div id="game-container">
       <ol id="players">
-        <li>
-          <span className="player">
-              <span className="player-name">Player 1</span>
-              <span className="player-symbol">X</span>
-          </span>
-          <button>Edit</button>
-        </li>
-        <li>
-          <span className="player">
-              <span className="player-name">Player 2</span>
-              <span className="player-symbol">O</span>
-          </span>
-          <button>Edit</button>
-        </li>
+        <Player name="player 1" symbol="X" />
+        <Player name="player 2" symbol="O" />
       </ol>
       Game boared
     </div>
