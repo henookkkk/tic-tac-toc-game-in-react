efaa821464bd987fd76ed1b25dcfa114d6722040
Henok Dereje <henzdereje@gmail.com>
2026-03-16 07:15:53 -0700

    i made the edit button inside the player component interative using the state concept. When it is clicked the player can input a name he wants.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
+
+
 export default function Player({name, symbol}){
+    const [isEditing, setIsEditing] = useState(false);
+    function handleClick(){
+        setIsEditing(true);
+    }
+    let playerName = <span className="player-name">{name}</span>;
+    let buttonType = "Edit";
+    if(isEditing){
+        playerName = <input type="text" />
+        buttonType = "save";
+    }
     return(
         <li>
           <span className="player">
-              <span className="player-name">{name}</span>
+              {playerName}
               <span className="player-symbol">{symbol}</span>
           </span>
-          <button>Edit</button>
+          <button onClick={handleClick}>{buttonType}</button>
         </li>
     );
 }
